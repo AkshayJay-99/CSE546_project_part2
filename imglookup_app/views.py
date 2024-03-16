7812a694974736aeffbea532f6881cbdd8c1e061
Akshay <ajsoorya@gmail.com>
2024-03-16 12:57:18 -0700

    s3-upload added

--- imglookup_app/views.py
+++ imglookup_app/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from asgiref.sync import async_to_sync
 from imglookup_app.apps import df
+import boto3
+import os
 
 
 # Create your views here.
@@ -11,12 +13,27 @@ from imglookup_app.apps import df
 def imglookup(request):
     if request.method == 'POST' and 'inputFile' in request.FILES:
         input_file = request.FILES['inputFile']
-        input_file_withoutextension = input_file.name.split('.')[0]
-        filtered_df = df[df['Image']==input_file_withoutextension]
-        return_value = filtered_df.iloc[0]['Results']
-        return HttpResponse(f"{input_file.name.split('.')[0]}:{return_value}")
+        file_name = input_file.name
+        local_folder_path = 'imglookup_app/static/images'
+        local_file_path = os.path.join(local_folder_path,file_name)
+        
+        # Save the file to the local folder
+        with open(local_file_path, 'wb') as local_file:
+            local_file.write(input_file.read())
+            
+        s3_bucket = '1229059769-in-bucket'
+        
+        upload_folder_to_s3(local_file_path, s3_bucket, file_name)
+            
+        
     else:
         return HttpResponse("Invalid request")
+
+def upload_folder_to_s3(local_path, s3_bucket, s3_key):
+    s3 = boto3.client('s3')
+    #uploading images to the s3 bucket
+    s3.upload_file(local_path, s3_bucket, s3_key)
+
 
 
 
